78fe254dbd604a35efdcde027f42bacd7aa328ff
fren43051 <32681314+fren43051@users.noreply.github.com>
2024-07-09 20:55:40 -0600

    implementando los metodos http

--- foro_hub/src/main/java/com/aluracursos/foro_hub/auth/SecurityConfig.java
+++ foro_hub/src/main/java/com/aluracursos/foro_hub/auth/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.aluracursos.foro_hub.auth;
 
-
 import com.aluracursos.foro_hub.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -31,9 +35,11 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .requestMatchers("/topico/cerrados").authenticated()
                         .requestMatchers("/topico/**").authenticated()
                         .requestMatchers("/login").permitAll()
+                        .requestMatchers("/usuarios").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
@@ -52,5 +58,16 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- foro_hub/src/main/java/com/aluracursos/foro_hub/service/UsuarioService.java
+++ foro_hub/src/main/java/com/aluracursos/foro_hub/service/UsuarioService.java
@@ -5,6 +5,8 @@ import com.aluracursos.foro_hub.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UsuarioService {
 
@@ -20,6 +22,9 @@ public class UsuarioService {
             throw new IllegalArgumentException("El correo electrónico ya está en uso.");
         }
         return usuarioRepository.save(usuario);
+    }
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
     }
 }
 
